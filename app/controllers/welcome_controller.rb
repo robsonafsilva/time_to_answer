a3f7c2360324aea6e8b09b543d97aad471225753
Robson <robsonap.silva@gmail.com>
2022-10-05 12:49:27 -0300

    first commit

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+  # http_basic_authenticate_with name: "robson", password: "123"
+  def index
+  end
+end
---
+++ config/routes.rb
@@ -0,0 +1,10 @@
+Rails.application.routes.draw do
+  devise_for :profiles
+  devise_for :admins
+  get 'welcome/index'
+  #Mapeamento personalizado
+  get 'inicio', to: 'welcome#index'
+
+  root to: 'welcome#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
